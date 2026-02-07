a961cf2cce95ad544c5713b7eaaa01edd000d324
Soumen Bhuin <smbhuin@gmail.com>
2026-02-07 23:14:56 +0530

    fix: unable to run test cases

--- main.go
+++ main.go
@@ -10,52 +10,40 @@ import (
 const defaultConfigFile = "./config.json"
 const defaultDatabaseFile = "./quickstore.db"
 
-var configFile string
-var databaseFile string
-
 var rootMux *http.ServeMux
 
-func init() {
-	flag.StringVar(&configFile, "config", defaultConfigFile, "Path to config file")
-	flag.StringVar(&databaseFile, "db", defaultDatabaseFile, "Path to database file")
-	flag.Parse()
-
+func initApp(configFile string, databaseFile string) error {
 	var err error
+
 	config, err = readConfig(configFile)
 	if err != nil {
-		log.Fatalf("Could not read config file: %v", err)
+		return fmt.Errorf("could not read config file: %w", err)
 	}
-
-	log.Println("Read config file successfully")
 
 	schemaCache = buildSchemaCache(config.Collections)
 	authCache = buildAuthCache(config)
 
 	openapiSpec, err = buildOpenapiSpec(config)
 	if err != nil {
-		log.Fatalf("Error creating OpenAPI spec: %v", err)
+		return fmt.Errorf("error creating OpenAPI spec: %w", err)
 	}
 
 	db, err = connectToDatabase(databaseFile)
 	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
+		return fmt.Errorf("error connecting to database: %w", err)
 	}
-
-	log.Println("Database connected successfully")
 
 	err = migrateDatabase(db, config.Collections)
 	if err != nil {
-		log.Fatalf("Error migrating database: %v", err)
+		return fmt.Errorf("error migrating database: %w", err)
 	}
 
-	log.Println("Database migrated successfully")
-
+	return nil
 }
 
 func registerRoutes() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /health", healthHandler)
-
 	mux.HandleFunc("OPTIONS /{collection}", mockOptionsHandler)
 	mux.HandleFunc("OPTIONS /{collection}/{id}", mockOptionsHandler)
 	mux.HandleFunc("GET /{collection}", getAllDocumentsHandler)
@@ -64,23 +52,32 @@ func registerRoutes() {
 	mux.HandleFunc("GET /{collection}/{id}", getDocumentHandler)
 	mux.HandleFunc("PUT /{collection}/{id}", replaceDocumentHandler)
 	mux.HandleFunc("DELETE /{collection}/{id}", deleteDocumentHandler)
-
 	apiMux := SetGlobalHeaders(mux)
-
 	rootMux = http.NewServeMux()
 	rootMux.Handle("/api/", http.StripPrefix("/api", apiMux))
 	rootMux.Handle("/docs/", http.StripPrefix("/docs", SwaggerHandler()))
-
 }
 
 func main() {
+	var configFile string
+	var databaseFile string
+
+	flag.StringVar(&configFile, "config", defaultConfigFile, "Path to config file")
+	flag.StringVar(&databaseFile, "db", defaultDatabaseFile, "Path to database file")
+	flag.Parse()
+
+	err := initApp(configFile, databaseFile)
+	if err != nil {
+		log.Fatalf("Startup error: %v", err)
+	}
+
 	defer db.Close()
 
 	registerRoutes()
 
 	log.Printf("QuickStore Server starting on http://%s:%d", config.Host, config.Port)
 
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", config.Host, config.Port), rootMux)
+	err = http.ListenAndServe(fmt.Sprintf("%s:%d", config.Host, config.Port), rootMux)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
